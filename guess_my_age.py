4775dea6a27015be3989a192ae892c8c8bd61e77
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-09-09 19:51:40 -0400

    Update guess_my_age.py

--- guess_my_age.py
+++ guess_my_age.py
@@ -1,4 +1,4 @@
-number = int(input("pick a number between 1 and 10: "))
+number = int(input("Pick a number between 1 and 10: "))
 year_born0 = int(input("If you've already had a birthday this year, enter 1772. Otherwise, enter 1771: "))
 new_number = (number*2 + 5)*50
 
@@ -6,7 +6,7 @@ def subtract(born):
     year_born1 = int(input("Enter the year that you were born: "))
     i = born-year_born1
     i_converted = str(i)
-    print("The magic number is \"" + i_converted + ".\" That means you are " + i_converted[1:3] + "!")
+    print("The magic number is \"" + i_converted + "\". That means you are " + i_converted[1:3] + "!")
 
 
 if year_born0 == 1772:
@@ -15,3 +15,8 @@ if year_born0 == 1772:
 else:
     new_number += 1771
     subtract(new_number)
+
+
+
+
+
