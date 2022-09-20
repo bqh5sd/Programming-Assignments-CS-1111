d02f87e024a703ac3f4e2ace0c6f73578428c864
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-09-19 21:33:51 -0400

    Update heart_rate.py

--- heart_rate.py
+++ heart_rate.py
@@ -3,15 +3,21 @@
 
 #Estimate the heart rate in bearts per minute (bmp)
 def gellish2(age):
-    #Gellish2 equation
+    '''
+    Use gellish2 equation to calculate estimated heart rate
+    return heart rate as a float
+    '''
     est_HRmax = 191.5 - (0.007*age**2)
     return float(est_HRmax)
 
 #Tests whether or not the estimated heart rate is in target range between 65% and 85% which is desired range of heart rate
 def in_target_range(hr, age):
+    '''
+    Set a range to test against the estimated heart rate from the gellish2 equation
+    return a boolean data type by using logical operators
+    '''
     target_range_lower = 0.65*gellish2(age)
     target_range_upper = 0.85*gellish2(age)
-    #Set range to test against hr 
     range_result = (target_range_lower < hr < target_range_upper)
     return range_result
 
