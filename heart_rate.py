3eb6e492c2c4d37e01cf3e56fa92942addcc01f5
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-09-23 23:46:05 -0400

    Update heart_rate.py

--- heart_rate.py
+++ heart_rate.py
@@ -1,26 +1,52 @@
 #Omid Akbari (bqh5sd)
 
-
-#Estimate the heart rate in bearts per minute (bmp)
-def gellish2(age):
+#Calculate fine for speeding
+def fine(speed_limit, my_speed, zone = "None"):
     '''
-    Use gellish2 equation to calculate estimated heart rate
-    return heart rate as a float
+    This function calculates the fine for speeding given the speed limit that is set and the speed
+    that is being recorded for an individual. This function also consider the city zone in which this
+    calculation is based off of.
+    :param speed_limit: The integer speed limit that is set by the city
+    :param my_speed: Integer speed which is being recorded by yourself 
+    :param zone: A string which denotes the type of zone you are in for the given calculation
+    :return: Returns the total dollar integer amount of money that you owe
     '''
-    est_HRmax = 191.5 - (0.007*age**2)
-    return float(est_HRmax)
+    amount_due = 0
+    speed_limit_difference = my_speed - speed_limit
+    if speed_limit_difference > 0 and speed_limit_difference < 20:
+        if "residential" in zone:
+            print(zone)
+            amount_due = (speed_limit_difference*8)+200
+        elif zone == "school" or zone == "work":
+            amount_due = speed_limit_difference*7
+        else:
+            amount_due = speed_limit_difference*6
+    else:
+        if speed_limit_difference >= 20:
+            amount_due += 350
+        elif speed_limit_difference < -10:
+            amount_due += 30
+    return amount_due
 
-#Tests whether or not the estimated heart rate is in target range between 65% and 85% which is desired range of heart rate
-def in_target_range(hr, age):
+#Calculate demerits points for speeding
+def demerits(speed_limit, my_speed):
     '''
-    Set a range to test against the estimated heart rate from the gellish2 equation
-    return a boolean data type by using logical operators
+    The function calculates the total demerit points earned for speeding under some 
+    pre-determined ranges
+    
+    :param speed_limit: The integer speed limit that is set by the city
+    :param my_speed: Integer speed which is being recorded by yourself
+    :return: Returns the total integer demerit point earned for speeding violations
     '''
-    target_range_lower = 0.65*gellish2(age)
-    target_range_upper = 0.85*gellish2(age)
-    range_result = (target_range_lower < hr < target_range_upper)
-    return range_result
-
-
-
+    speed_limit_difference = my_speed - speed_limit
+    demerits_points = 0
+    if speed_limit_difference >= 1 and speed_limit_difference <=9:
+        demerits_points += 3
+    elif speed_limit_difference >= 10 and speed_limit_difference <= 19:
+        demerits_points += 4
+    elif speed_limit_difference >= 20:
+        demerits_points += 6
+    else:
+        demerits_points = 0
+    return demerits_points
 
