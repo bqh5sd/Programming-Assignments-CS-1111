4d757926179bc26932f18261ee843b8d19bf5e49
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-09-30 22:34:52 -0400

    Create pocket_calculator.py

---
+++ pocket_calculator.py
@@ -0,0 +1,48 @@
+current_value = 0
+operation_performed = ""
+recent_value = 0
+expression = str(current_value)
+
+def get_value():
+    return current_value
+
+def clear(clr_input = 0):
+    global current_value
+    global recent_value
+    global expression
+    global operation_performed
+    current_value = clr_input
+    recent_value = 0
+    expression = str(current_value)
+    operation_performed = "0"
+    return current_value
+
+def step(operation_input, value_input):
+    global current_value
+    # For reptition:
+    global recent_value
+    global operation_performed
+    global expression
+
+    recent_value = value_input
+    operation_performed = operation_input
+
+    expression = "(" + expression + ")" + operation_performed + str(recent_value)
+
+    if operation_input == "+":
+        current_value += value_input
+    elif operation_input == "-":
+        current_value -= value_input
+    elif operation_input == "*":
+        current_value *= value_input
+    else:
+        current_value = current_value/value_input
+
+    return int(current_value)
+
+def repeat():
+    step(operation_performed, recent_value)
+    return int(current_value)
+
+def get_expr():
+    return expression
