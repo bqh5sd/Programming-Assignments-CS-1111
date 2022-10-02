fe204d7ce9ff5315d24faa21e438160f3ca2f4a8
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-10-02 13:06:57 -0400

    Update quadrant.py

--- quadrant.py
+++ quadrant.py
@@ -4,6 +4,14 @@
 
 
 def quad(x, y):
+    '''
+    :param x: input of x coordinate with magnitude and sign
+    :param y: input of y coordinate with magnitude and sign
+    :return: Returns the quadrant where x and y coordinate of a single point lies on the xy plane
+
+    '''
+    # Check conditions of x and y using logic, where a specific quadrant must contain certain x and y values
+    # if x or y are 0, or both are zero, then a quadrant DNE and will return 0
     if x > 0 and y > 0:
         quadrant = 1
     elif x < 0 and y > 0:
