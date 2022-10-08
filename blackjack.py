56101429272a0634709d7fed2458be53c2a1b28c
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-10-07 21:00:27 -0400

    Create blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,78 @@
+
+
+
+def card_to_value(card):
+    #Card is a string
+    #returns the numeric blackjack score of the card
+    #For "A", this should return 1, not 11
+    #For the rest face cards, it should return 10
+    card_faces = ["T", "J", "Q", "K"]
+    card_values = ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+    if card == "A":
+        score = 1
+        return 1
+
+    for cards in card_values:
+        if cards == card:
+            score = int(card)
+            return score
+
+    for cards in card_faces:
+        if cards == card:
+            score = 10
+            return score
+
+def hard_score(hand):
+    i = 0
+    score = 0
+
+    card_faces = ["T", "J", "Q", "K"]
+    card_values = ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+    while i < (len(hand)):
+        for letters in hand[i]:
+            i += 1
+            #print(letters)
+            if letters == "A":
+                score += 1
+            for cards in card_faces:
+                if cards == letters:
+                    score += 10
+            for cards in card_values:
+                if cards == letters:
+                    score += int(letters)
+    return score
+
+
+def soft_score(hand):
+    i = 0
+    score = 0
+    a_count = 0
+
+    card_faces = ["T", "J", "Q", "K"]
+    card_values = ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+    while i < (len(hand)):
+        for letters in hand[i]:
+            i += 1
+            # print(letters)
+            if letters == "A":
+                a_count += 1
+                if a_count > 1:
+                    Ace_score = 1
+                else:
+                    Ace_score = 11
+                score += Ace_score
+            for cards in card_faces:
+                if cards == letters:
+                    score += 10
+            for cards in card_values:
+                if cards == letters:
+                    score += int(letters)
+
+    return score
+
+
+
+
