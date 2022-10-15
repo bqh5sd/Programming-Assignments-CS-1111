1732febde492d30421f2408a34114838e05c84d1
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-10-15 16:43:19 -0400

    Update similar.py

--- similar.py
+++ similar.py
@@ -1,4 +1,12 @@
+#Omid Akbari
+#bqh5sd
+
 def compare(list):
+    '''
+    This function compares the first and last character of a string in a list of strings to test if they are the same character
+    :param list: This is the list being tested with various string within
+    :return: Returns the total number of strings which have their first and last character the same
+    '''
     strings_similar = 0
     for string in list:
         first_character = string[0]
