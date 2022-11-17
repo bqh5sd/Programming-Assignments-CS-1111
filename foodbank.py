24d555c86d9d65d6516eb359cecbb1fbae53d2ea
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-11-17 15:46:19 -0500

    Update foodbank.py

--- foodbank.py
+++ foodbank.py
@@ -1,7 +1,18 @@
-def maxServed(fptr, year, served):
-    data = fptr.read().strip().split('\n')
+#bqh5sd
+#Omid Akbari
 
-    dict = {}
+
+def maxServed(fptr, year, served):
+    '''
+    The purpose of this function is to find the locality within virginia that has either the maximum number
+    of household served, individuals served or children served depending on the intrest
+    :param fptr: This parameter takes in file data that contains various forms of data
+    :param year: This parameter takes in the year of interest
+    :param served: This parameter takes in the quirey of interest where 1 is households served, 2 is individuals
+    served and 3 is the children served
+    :return:
+    '''
+    data = fptr.read().strip().split('\n')
 
     ref = -1
     max_locality = ""
