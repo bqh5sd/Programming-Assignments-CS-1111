c5c4b78e5c484736f4a35efd0f53aa6d1490837c
bqh5sd <112827660+bqh5sd@users.noreply.github.com>
2022-11-17 13:41:59 -0500

    Update foodbank.py

--- foodbank.py
+++ foodbank.py
@@ -1,12 +1,22 @@
 #Omid Akbari
 #bqh5sd
 
-
-def maxServed(fptr, year=1, served=1):
-    data = fptr.read().strip().split('\n')
+def find_max(dict):
 
     ref = -1
     max_locality = ""
+
+    for local, value in dict.items():
+        if value > ref:
+            ref = value
+            max_locality = local
+    return max_locality
+
+
+def maxServed(fptr, year, served=1):
+    data = fptr.read().strip().split('\n')
+
+    dict = {}
 
     for current_data in data[1:]:
         current_data = current_data.split(",")
@@ -14,19 +24,23 @@ def maxServed(fptr, year=1, served=1):
             if current_data[i] == "":
                 current_data[i] = "0"
         if served == 0:
-            if int(current_data[3]) > ref:
-                ref = int(current_data[3])
-                max_locality = current_data[2]
+            if int(current_data[0]) == year:
+                if current_data[2] in dict:
+                    dict[current_data[2]] = dict[current_data[2]] + int(current_data[3])
+                else:
+                    dict[current_data[2]] = int(current_data[3])
+
         if served == 1:
-            if int(current_data[4]) > ref:
-                ref = int(current_data[4])
-                max_locality = current_data[2]
+            if int(current_data[0]) == year:
+                if current_data[2] in dict:
+                    dict[current_data[2]] = dict[current_data[2]] + int(current_data[4])
+                else:
+                    dict[current_data[2]] = int(current_data[4])
         if served == 2:
-            if int(current_data[6]) > ref:
-                ref = int(current_data[6])
-                max_locality = current_data[2]
+            if int(current_data[0]) == year:
+                if current_data[2] in dict:
+                    dict[current_data[2]] = dict[current_data[2]] + int(current_data[6])
+                else:
+                    dict[current_data[2]] = int(current_data[6])
 
-
-    return max_locality
-
-
+    return find_max(dict)
